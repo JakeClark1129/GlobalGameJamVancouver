43fc7f8613de117c8e6b68887350c58fa62c1f80
moskatero <jkescoobar@gmail.com>
2016-01-29 22:52:29 -0800

    Initial Unity Commit
    Basic Character prefab created

---
+++ Ritualize/Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour 
+{
+
+	private CharacterController controller;
+	private Transform charTransform;
+	private Camera charCam;
+
+	// Use this for initialization
+	void Start () 
+	{
+		controller = GetComponent<CharacterController> ();
+		charTransform = GetComponent<Transform> ();
+		charCam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			controller.Move (charTransform.forward); 
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			controller.Move (-charTransform.forward); 
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			controller.Move (charTransform.right); 
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			controller.Move (-charTransform.right); 
+		}
+
+		Vector3 mousePos = Input.mousePosition;
+
+	}
+}
