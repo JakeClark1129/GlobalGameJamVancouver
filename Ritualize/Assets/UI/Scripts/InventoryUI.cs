873f6698d016ebd93913b82ec0693a635af821f2
Jason Estey <esteyj@yahoo.com>
2016-01-30 15:48:19 -0800

    inventory UI prefab

--- Ritualize/Assets/UI/Scripts/InventoryUI.cs
+++ Ritualize/Assets/UI/Scripts/InventoryUI.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class InventoryUI : MonoBehaviour
 {
+    public Inventory PlayerInventory;
+
     // TODO: need this method?
     public void HandleAlchemyCircleEnabled(AlchemyCircleUI alchemyCircle)
     {
