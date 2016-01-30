8c0388f2665c69121bfdc7571bd32263f516b345
Jason Estey <esteyj@yahoo.com>
2016-01-30 13:49:09 -0800

    created scriptable object for inventory item data

--- Ritualize/Assets/Scripts/Inventory.cs
+++ Ritualize/Assets/Scripts/Inventory.cs
@@ -1,24 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
-
+﻿
+using UnityEngine;
 
 public enum Items
 {
-	//Collectables
-	Herb = 1,
-	Flower = 1 << 1,
-	Root = 1 << 2,
-	Charcoal = 1 << 3,
-	Jar = 1 << 4,
-	Stick = 1 << 5,
-	Rock = 1 << 6,
+    //Collectables
+    Herb = 1,
+    Flower = 1 << 1,
+    Root = 1 << 2,
+    Charcoal = 1 << 3,
+    Jar = 1 << 4,
+    Stick = 1 << 5,
+    Rock = 1 << 6,
 
-	//Craftables
-	ElixerOfLight = 1 << 7,
-	SoulSnatcher = 1 << 8,
+    //Craftables
+    ElixerOfLight = 1 << 7,
+    SoulSnatcher = 1 << 8,
 }
 
 public class Inventory : MonoBehaviour 
 {
-	public Items inventory {get; private set;}
+    public Items inventory {get; private set;}
+
+    public InventoryItem[] ItemData;
+
+    public InventoryItem GetItemData(Items ID)
+    {
+        foreach (InventoryItem item in ItemData)
+        {
+            if (item.ID == ID)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
 }
