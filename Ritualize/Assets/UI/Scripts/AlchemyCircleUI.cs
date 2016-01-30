bdabd2bea6457eb83cc066b358907f545175ab77
Jason Estey <esteyj@yahoo.com>
2016-01-30 10:30:36 -0800

    added Alchemy Circle UI work in progress

---
+++ Ritualize/Assets/UI/Scripts/AlchemyCircleUI.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlchemyCircleUI : MonoBehaviour
+{
+    public InventoryUI _InventoryUI;
+
+    private void OnEnable()
+    {
+        if (_InventoryUI != null)
+        {
+            _InventoryUI.HandleAlchemyCircleEnabled(this);
+        }
+    }
+
+    public void HandleButtonClick(Button button)
+    {
+        Debug.Log("button was clicked: " + button);
+    }
+}
---
+++ Ritualize/Assets/UI/Scripts/InventoryUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryUI : MonoBehaviour
+{
+    // TODO: need this method?
+    public void HandleAlchemyCircleEnabled(AlchemyCircleUI alchemyCircle)
+    {
+
+    }
+}
