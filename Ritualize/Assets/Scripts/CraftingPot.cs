20bdfc3bcd4eb1cf699e12b0e1a0aad9e42c7343
Jason Estey <esteyj@yahoo.com>
2016-01-31 12:26:52 -0800

    revert some scripts to pre-merge

--- Ritualize/Assets/Scripts/CraftingPot.cs
+++ Ritualize/Assets/Scripts/CraftingPot.cs
@@ -4,25 +4,39 @@ using System.Collections.Generic;
 
 public class CraftingPot : MonoBehaviour
 {
-	Items m_Ingredients;
+    public GameObject Player;
 
-	[SerializeField]
-	Craftable[] m_craftables;
+    public Items m_Ingredients;
 
-	void AddIngredient(Items ingredient)
-	{
-		m_Ingredients |= ingredient;
-	}
+    [SerializeField]
+    Craftable[] m_craftables;
 
-	void Craft(GameObject player)
-	{
-		foreach( Craftable craftable in m_craftables)
-		{
-			if(craftable.m_RequiredItems == m_Ingredients)
-			{
-				Items inventory = player.GetComponent<Inventory>().inventory; 
-				craftable.Craft(inventory);
-			}
-		}
-	}
+    public void AddIngredient(Items ingredient)
+    {
+        m_Ingredients |= ingredient;
+    }
+
+    public void Clear()
+    {
+        m_Ingredients = 0;
+    }
+
+    public bool Craft()
+    {
+        Inventory inventory = Player.GetComponent<Inventory>();
+        if (!inventory)
+        {
+            return false;
+        }
+
+        foreach (Craftable craftable in m_craftables)
+        {
+            if (craftable.m_RequiredItems == m_Ingredients)
+            {
+                return craftable.Craft(inventory);
+            }
+        }
+
+        return false;
+    }
 }
